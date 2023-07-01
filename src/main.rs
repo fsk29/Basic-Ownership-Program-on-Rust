2f5f4d54a428c0045ccbe4a58357fec9f10a875f
Fatih Sevban KARTAL <87607332+fsk29@users.noreply.github.com>
2023-07-01 13:56:02 +0300

    ilk

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,12 @@
 fn main() {
-//    let message = "hello world";
-//    let x: i32 = 42;
-//    let pi : f64 =3.14;
-//    let is_rust_fun: bool = true;
-//    let letter_a : char ='a';
-   fn add( x: i32, y: i32) -> i32{
-        x+y 
-   }
-   let result:i32 = add(32, 32);
-   result.to_string();
-   println!("{}", result);
+    let s1 = String::from("hello");
 
+    let len = calculate_length(&s1);
 
-   if result < 0 {
-    println!("result is negative");
-   }
-   else {
-       println!("result is positive");
-   }
-
-
-   let mut i = 1;
-
-
-while i <= 5 {
-println!("{}", i);
-      i += 1;
+    println!("The length of '{}' is {}.", s1, len);
 }
 
-
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
+
